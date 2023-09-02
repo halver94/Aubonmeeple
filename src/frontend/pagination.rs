80309a001638043c118d3efbd3a720edf9d46716
halver94 <laplonge61@hotmail.com>
2023-09-02 23:00:43 +0200

    Fixed pagination

--- src/frontend/pagination.rs
+++ src/frontend/pagination.rs
@@ -58,8 +58,14 @@ pub fn generate_pagination_links(total_items: usize, state: &State) -> String {
 .pagination a:hover:not(.active) {background-color: #ddd;}
     </style>"#;
 
+    let min_visible_pages = 2;
+    let offset = 1;
+    let current_page = state.pagination.page;
+    let ellipsis = "<a>...</a>".to_string();
     let mut pagination_html = String::from(style);
     pagination_html.push_str(r#"<center><div class="pagination">"#);
+
+    // previous button
     if state.pagination.page != 0 {
         pagination_html.push_str(&format!(
             r#"<a href="/?{}&page={}&per_page={}">Previous</a>"#,
@@ -68,20 +74,45 @@ pub fn generate_pagination_links(total_items: usize, state: &State) -> String {
             state.pagination.per_page,
         ));
     }
-    for page in 0..=total_pages - 1 {
-        pagination_html.push_str(&format!(
-            r#"<a {} href="/?{}&page={}&per_page={}">{}</a>"#,
-            if page == state.pagination.page {
-                r#"class="active""#
-            } else {
-                ""
-            },
-            params,
-            page,
-            state.pagination.per_page,
-            page,
-        ));
+
+    for page in 0..total_pages {
+        if page < min_visible_pages || page > total_pages - min_visible_pages - 1 {
+            println!("min visible pages");
+            pagination_html.push_str(&format!(
+                r#"<a {} href="/?{}&page={}&per_page={}">{}</a>"#,
+                if page == current_page {
+                    r#"class="active""#
+                } else {
+                    ""
+                },
+                params,
+                page,
+                state.pagination.per_page,
+                page,
+            ));
+        } else if (current_page > offset && page < current_page - offset)
+            || page > current_page + offset
+        {
+            if !pagination_html.ends_with(&ellipsis) {
+                pagination_html.push_str(&ellipsis);
+            }
+        } else {
+            pagination_html.push_str(&format!(
+                r#"<a {} href="/?{}&page={}&per_page={}">{}</a>"#,
+                if page == current_page {
+                    r#"class="active""#
+                } else {
+                    ""
+                },
+                params,
+                page,
+                state.pagination.per_page,
+                page,
+            ));
+        }
     }
+
+    // next button
     if state.pagination.page != total_pages - 1 {
         pagination_html.push_str(&format!(
             r#"<a href="/?{}&page={}&per_page={}">Next</a>"#,
@@ -92,5 +123,6 @@ pub fn generate_pagination_links(total_items: usize, state: &State) -> String {
     }
     pagination_html.push_str("</div></center>");
 
+    println!("{}", pagination_html);
     pagination_html
 }
