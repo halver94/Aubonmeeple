e602e6df9408527c471283398cc9c00ca5511228
halver94 <laplonge61@hotmail.com>
2023-09-23 12:09:25 +0200

    Splitted into 2 binaries

--- src/frontlib/mod.rs
+++ src/frontlib/mod.rs
@@ -1,6 +1,7 @@
 pub mod filter;
 pub mod footer;
 pub mod frontpage;
+pub mod header;
 pub mod pagination;
 pub mod server;
 pub mod sort;
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod db;
+pub mod frontlib;
+pub mod game;
+pub mod website;
