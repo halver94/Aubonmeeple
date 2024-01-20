92e94a273466d6aaae3d242c41cf70a2b104d379
Alexandre Boucey <alexandre.boucey@gmail.com>
2024-01-21 00:32:27 +0100

    feat: use a different listener for prometheus metrics

--- src/bin/frontend/main.rs
+++ src/bin/frontend/main.rs
@@ -1,11 +1,18 @@
+use tokio::join;
 use boardgame_finder::frontlib::server;
+use boardgame_finder::metrics;
 
 #[tokio::main]
 async fn main() {
     log_panics::init();
     env_logger::init();
 
+    let frontend_bind_addr = std::env::var("FRONTEND_ADDR").unwrap_or("0.0.0.0:3001".to_string());
+    let frontend_metrics_bind_addr = std::env::var("FRONTEND_METRICS_ADDR").unwrap_or("127.0.0.1:3002".to_string());
+
     log::info!("[MAIN] starting program");
-    let join = tokio::spawn(async move { server::set_server().await });
-    let _ = join.await;
+    join!(
+        server::run_server(frontend_bind_addr),
+        metrics::run_metrics(frontend_metrics_bind_addr),
+    );
 }
